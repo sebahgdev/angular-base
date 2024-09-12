43e6d83507c1661f51a3e3a694817210811ea497
Sebastian <sebahg.dev@gmail.com>
2024-09-12 11:43:40 -0300

    correccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
 
 
   ],
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,25 @@
+
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+
+
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+  ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    this.onDelete.emit(id);
+  }
+ }
